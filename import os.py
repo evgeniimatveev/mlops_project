a1e986bbd4a30946ddb21d5b1701f28671949ca4
Jay <evgeniimatveevusa@gmail.com>
2025-03-19 11:42:05 -0700

    Fix formatting issues

--- import os.py
+++ import os.py
@@ -1,15 +1,17 @@
 import os
 import re
 
+
 def remove_russian_comments(directory):
-    
-        for file in files:
-                path = os.path.join(root, file)
-                with open(path, "r", encoding="utf-8") as f:
-                    lines = f.readlines()
 
-                with open(path, "w", encoding="utf-8") as f:
-                    for line in lines:
-                            f.write(line)
+    for file in files:
+        path = os.path.join(root, file)
+        with open(path, "r", encoding="utf-8") as f:
+            lines = f.readlines()
 
-remove_russian_comments(".")  
+        with open(path, "w", encoding="utf-8") as f:
+            for line in lines:
+                f.write(line)
+
+
+remove_russian_comments(".")
